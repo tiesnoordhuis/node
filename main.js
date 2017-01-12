053f490f707d6414535cf7ffd88901dacf71c3a3
Ties Noordhuis <tiesnoordhuis@gmail.com>
2017-01-12 15:06:51 +0100

    first

---
+++ main.js
@@ -0,0 +1,24 @@
+/* mijn eerste node.js server */
+const http = require("http");
+const fs = require("fs");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+fs.readFile("../Bellwald/index.html", (err, html) => {
+  if (err){
+    throw err;
+  }
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type","text/html");
+    res.write(html);
+    res.end();
+  });
+
+  server.listen(port, hostname, () =>{
+    console.log("server started on "+hostname+":"+port );
+  });
+
+});
