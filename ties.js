92092e1d785caea8b91f1fdd478ed2737d671b3e
Ties Noordhuis <tiesnoordhuis@gmail.com>
2017-02-08 00:28:51 +0100

    whattup

--- ties.js
+++ ties.js
@@ -7,7 +7,7 @@ console.log("eerste regel");
 var tileNames = ["nummer", "naam", "waarde", "bestaat"];
 var tileNummer = [1, 2, 3, 4, 5, 6];
 var tileNaam = ["title1", "title2", "title3", "title4", "title5", "title6"];
-var tileWaarde = [50, 40, 70, 20, 50, 00];
+var tileWaarde = [55, 40, 70, 20, 50, 00];
 var tileBestaat = [true, true, true, true, true, false];
 var tileProperties = [tileNummer, tileNaam, tileWaarde, tileBestaat];
 
@@ -25,23 +25,16 @@ var tile3 = tile(2);
 var tile4 = tile(3);
 var tile5 = tile(4);
 var tile6 = tile(5);
-
-//var tile1 = {tilenames[1]:1, tilenames[2]:"tile1", tilenames[3]:50, tilenames[4]:true};
-//var tile2 = {tilenames[1]:2, tilenames[2]:"tile2", tilenames[3]:40, tilenames[4]:true};
-//var tile3 = {tilenames[1]:3, tilenames[2]:"tile3", tilenames[3]:70, tilenames[4]:true};
-//var tile4 = {tilenames[1]:4, tilenames[2]:"tile4", tilenames[3]:20, tilenames[4]:true};
-//var tile5 = {tilenames[1]:5, tilenames[2]:"tile5", tilenames[3]:50, tilenames[4]:true};
-//var tile6 = {tilenames[1]:6, tilenames[2]:"tile6", tilenames[3]:00, tilenames[4]:false};
-
 var tiles = [tile1, tile2, tile3, tile4, tile5, tile6];
 
 console.log(tileNames);
 
-console.log(tile2);
+console.log(tileProperties);
 
 console.log(tiles);
 
-/*
+console.log(tiles[3]["waarde"]);
+
 var inputData = fs.readFile("ties.txt", (err, inputData) => {
   if (err) {
     return console.error(err);
@@ -51,18 +44,59 @@ var inputData = fs.readFile("ties.txt", (err, inputData) => {
   }
 });
 
-fs.writeFile('ties2.txt', writeTiles(), (err) => {
+fs.writeFile('ties2.txt', writeTiles(), "utf8", (err) => {
     if (err) throw err;
     console.log('It\'s saved!');
   });
 
-/*function writeTiles(){
+function writeTiles(){
   var string = "";
   for (var i1 = 0; i1 < tiles.length; i1++) {
     for (var i2 = 0; i2 < (tileNames.length); i2++) {
-      string +=  tiles[i1][tileNames[i2]];
+      string +=  tiles[i1][tileNames[i2]] + " , ";
     }
+  string += "\r\n";
   }
   return string;
 }
-*/
+
+function ageCycle() {
+  var waardeNu = [];
+  var waardeNuExtended = [];
+  for (var i = 0; i < tiles.length; i++) {
+    waardeNu[i] = tiles[i]["waarde"];
+    waardeNuExtended[i] = tiles[i]["waarde"];
+  }
+  var temp = waardeNu[waardeNu.length-1];
+  waardeNuExtended.splice(0,0,temp);
+  waardeNuExtended.push(waardeNu[0]);
+  var waardeNuRest = [];
+  for (var i = 0; i < tiles.length; i++) {
+    waardeNuRest[i] = ((waardeNuExtended[i] + waardeNuExtended[i+2])/2);
+  }
+  for (var i = 0; i < waardeNu.length; i++) {
+    if (waardeNu[i] <= waardeNuRest[i]) {
+      console.log("if statement: "+ waardeNu[i] +" "+ waardeNuRest[i]);
+      waardeNu[i] ++;
+    }
+    else {
+      waardeNu[i] --;
+    }
+  }
+  console.log("waardeNu: "+waardeNu);
+  return waardeNu;
+}
+
+function doCycle() {
+for (var i = 0; i < tiles.length; i++) {
+  var waardeNu = ageCycle();
+  tiles[i]["waarde"] = waardeNu[i];
+  if (i == tiles.length-1) {
+    console.log(tiles);
+  }
+}
+}
+
+for (var i = 0; i < 200; i++) {
+  doCycle();
+}
