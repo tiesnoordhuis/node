848a57c19d3e653f851ca795710e09d8701081a4
Ties Noordhuis <tiesnoordhuis@gmail.com>
2017-02-07 15:15:44 +0100

    het werkt?

--- ties.js
+++ ties.js
@@ -4,11 +4,27 @@ var fs = require("fs");
 
 console.log("eerste regel");
 
-var tilenames = ["true", "nummer", "naam", "waarde", "bestaat"];
+var tileNames = ["nummer", "naam", "waarde", "bestaat"];
+var tileNummer = [1, 2, 3, 4, 5, 6];
+var tileNaam = ["title1", "title2", "title3", "title4", "title5", "title6"];
+var tileWaarde = [50, 40, 70, 20, 50, 00];
+var tileBestaat = [true, true, true, true, true, false];
+var tileProperties = [tileNummer, tileNaam, tileWaarde, tileBestaat];
 
-var tile1 = {};
-tile1[tilenames[1]] = "1";
-tile1[tilenames[2]] = "tile1";
+function tile(n) {
+  var tile = {};
+  for (var i = 0; i < tileNames.length; i++) {
+    tile[tileNames[i]] = tileProperties[i][n];
+  }
+  return tile;
+}
+
+var tile1 = tile(0);
+var tile2 = tile(1);
+var tile3 = tile(2);
+var tile4 = tile(3);
+var tile5 = tile(4);
+var tile6 = tile(5);
 
 //var tile1 = {tilenames[1]:1, tilenames[2]:"tile1", tilenames[3]:50, tilenames[4]:true};
 //var tile2 = {tilenames[1]:2, tilenames[2]:"tile2", tilenames[3]:40, tilenames[4]:true};
@@ -19,16 +35,13 @@ tile1[tilenames[2]] = "tile1";
 
 var tiles = [tile1, tile2, tile3, tile4, tile5, tile6];
 
-var arr = ["hier1", "hier2"];
+console.log(tileNames);
+
+console.log(tile2);
 
 console.log(tiles);
 
-console.log(tiles[1]);
-
-console.log(tiles[2]["naam"]);
-
-console.log(tiles["waarde"]);
-
+/*
 var inputData = fs.readFile("ties.txt", (err, inputData) => {
   if (err) {
     return console.error(err);
@@ -43,12 +56,13 @@ fs.writeFile('ties2.txt', writeTiles(), (err) => {
     console.log('It\'s saved!');
   });
 
-function writeTiles(){
+/*function writeTiles(){
   var string = "";
   for (var i1 = 0; i1 < tiles.length; i1++) {
-    for (var i2 = 0; i2 < (tilenames.length - 1); i2++) {
-      string +=  tiles[i1][tilenames[i2]];
+    for (var i2 = 0; i2 < (tileNames.length); i2++) {
+      string +=  tiles[i1][tileNames[i2]];
     }
   }
   return string;
 }
+*/
